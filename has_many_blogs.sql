cc6baf97533ef8f5a861b731222837c44d8f56fe
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-24 20:33:39 -1000

    up to 12 done

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,4 +1,4 @@
-/*\c postgres
+\c postgres
 
 DROP DATABASE IF EXISTS has_many_blogs;
 DROP USER IF EXISTS has_many_user;
@@ -6,19 +6,20 @@ DROP USER IF EXISTS has_many_user;
 
 CREATE USER has_many_user;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
-\c has_many_blogs*/
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS posts;
+\c has_many_blogs
+
 DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(90) NOT NULL,
   first_name VARCHAR(90) DEFAULT NULL,
   last_name VARCHAR(90) DEFAULT NULL,
-  created_at timestamp without time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp without time zone NOT NULL DEFAULT NOW()
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE posts (
@@ -26,16 +27,16 @@ CREATE TABLE posts (
   title VARCHAR(180) DEFAULT NULL,
   url VARCHAR(510) DEFAULT NULL,
   content TEXT DEFAULT NULL,
-  created_at timestamp without time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp without time zone NOT NULL DEFAULT NOW(),
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
   users_id INT REFERENCES users(id) NOT NULL
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   body VARCHAR(510) DEFAULT NULL,
-  created_at timestamp without time zone NOT NULL DEFAULT NOW(),
-  updated_at timestamp without time zone NOT NULL DEFAULT NOW(),
+  created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+  updated_at timestamp with time zone NOT NULL DEFAULT NOW(),
   users_id INT REFERENCES users(id) NOT NULL,
   posts_id INT REFERENCES posts(id) NOT NULL
 );
--- joins.sql
+++ joins.sql
@@ -1,12 +1,61 @@
-
-
-SELECT *
+/**SELECT *
 FROM users;
+
 
 SELECT *
 FROM posts
 WHERE posts.users_id = 100;
 
+
 SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name, u.last_name
 FROM posts p
 INNER JOIN users u ON u.id = 200;
+
+
+SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name
+FROM posts p
+INNER JOIN users u ON u.first_name = 'Norene' AND u.last_name = 'Schmitt';
+
+
+SELECT u.username
+FROM users u
+INNER JOIN posts p ON p.users_id = u.id
+WHERE p.created_at > '2015-01-01';
+
+
+SELECT p.title, p.content, u.username
+FROM posts p
+INNER JOIN users u ON u.id = p.users_id
+WHERE u.created_at < '2015-01-01';
+
+
+SELECT c.id, c.body, c.created_at, c.updated_at, p.title as "Post Title"
+FROM posts p
+RIGHT JOIN comments c ON c.posts_id = p.id;*/
+
+
+SELECT c.id, c.body as "comment body", c.created_at, c.updated_at, p.title as "post title", p.url as "post url"
+FROM comments c
+LEFT JOIN posts p ON p.id = c.posts_id
+WHERE p.created_at < '2015-01-01';
+
+
+SELECT c.id, c.body as "comment body", c.created_at, c.updated_at, p.title as "post title", p.url as "post url"
+FROM comments c
+LEFT JOIN posts p ON p.id = c.posts_id
+WHERE p.created_at > '2015-01-01';
+
+
+SELECT c.id, c.body as "comment body", c.created_at, c.updated_at, p.title as "post title", p.url as "post url"
+FROM comments c
+LEFT JOIN posts p ON c.posts_id = p.id
+WHERE c.body LIKE '%USB%';
+
+SELECT p.title as "post title", u.first_name, u.last_name, c.body as "comment_body"
+FROM users u
+INNER JOIN posts p ON p.users_id = u.id
+INNER JOIN comments c ON c.posts_id = p.id
+WHERE c.body LIKE '%matrix%';
+
+/*SELECT c.body as "comment_body"
+FROM comments c*/
